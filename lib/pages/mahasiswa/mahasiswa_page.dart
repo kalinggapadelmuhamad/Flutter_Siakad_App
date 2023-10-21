c8e571150a801f0d91b15ebc480c8c0e62336383
kalinggapadelmuhamad <enginerbros@gmail.com>
2023-10-21 22:18:10 +0700

    Initial Push

---
+++ lib/pages/mahasiswa/mahasiswa_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MahasiswaPage extends StatefulWidget {
+  const MahasiswaPage({super.key});
+
+  @override
+  State<MahasiswaPage> createState() => _MahasiswaPageState();
+}
+
+class _MahasiswaPageState extends State<MahasiswaPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
